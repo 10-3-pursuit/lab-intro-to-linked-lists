36ac325a759df6a684857f33f77cb45b0ccc8e1a
juligarc91 <julissagarcia@pursuit.org>
2024-05-21 13:47:14 -0400

    created class node with attributes data and next

--- solution.js
+++ solution.js
@@ -1,7 +1,12 @@
 const { nums, words } = require("./data/data.js");
-import { inspect } from "util"; // npm i util to install; to load this ES module, set "type": "module" in the package.json
-
-console.log(inspect())
+// import { inspect } from "util"; // npm i util to install; to load this ES module, set "type": "module" in the package.json
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+// console.log(inspect())
 module.exports = {
   Node,
   LinkedList,
