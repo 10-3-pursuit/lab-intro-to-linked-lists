bd274376455648f94ec0d5af02b4e32989810862
juligarc91 <julissagarcia@pursuit.org>
2024-05-21 12:27:33 -0400

    added inspect util package

--- solution.js
+++ solution.js
@@ -1,5 +1,7 @@
 const { nums, words } = require("./data/data.js");
+import { inspect } from "util"; // npm i util to install; to load this ES module, set "type": "module" in the package.json
 
+console.log(inspect())
 module.exports = {
   Node,
   LinkedList,
