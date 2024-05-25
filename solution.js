a33241d1ee11e39d241a9804634b7fb95e86b8a5
juligarc91 <julissagarcia@pursuit.org>
2024-05-25 12:42:34 -0400

    created method size to count length of linked list

--- solution.js
+++ solution.js
@@ -17,6 +17,15 @@ class LinkedList {
     newNode.next = this.head; // replace null value of node to make a new head to start LinkedList
     this.head = newNode; // makes new node the new beginning head
   }
+  size() { // counts length of linked list
+    let count = 0;
+    let node = this.head; // node becomes this.head with value of null at first;
+    while (node) { // count node while node isn't falsy (null in this case)
+      count++;
+      node = node.next; // once node is counted move to next node to see if it's falsy. If it is count it
+    }
+    return count; // return count once it reaches the end (once it reaches a node with value null which means there is no node there)
+  }
 }
 
 module.exports = { Node, LinkedList };
